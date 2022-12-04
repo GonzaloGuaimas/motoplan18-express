3524850866f4e6b95496dcbad98196871173c86f
gonzaloguaimas <gonzaloguaimas@gmail.com>
2022-12-04 10:52:35 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,51 @@
+//----Encabezado
+const express = require('express');
+const app = express();
+const bodyParser= require('body-parser')
+const MongoClient = require('mongodb').MongoClient
+const port = process.env.PORT || 3000
+
+//----Express
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static(__dirname + '/public'))
+app.listen(port, function() {
+    console.log('Enlazado a Puerto')
+})
+
+app.get('/', (req, res) => {
+  res.render('index')
+  
+})
+
+
+MongoClient.connect('mongodb+srv://FreeUser:freeqwerty@cluster0.zvq5w.mongodb.net/test', { useUnifiedTopology: true })
+  .then(client => {
+    console.log('Connected to Database')
+    //--------------------------OMEGA--------------------------
+    const db = client.db('Crefind')
+    const collectionClientes = db.collection('clientes')
+
+    app.get('/credenciales', (req, res) => {
+      res.render('credenciales.ejs')
+      
+    })
+
+    //----------FORMULARIO CLIE------------------------------------
+    app.post('/registrar', (req, res) => {
+      collectionClientes.insertOne(req.body)
+        .then(result => {
+          res.redirect('/')
+          console.log(result)
+        })
+        .catch(error => console.error(error))
+      })
+
+      //-----------------CV-------------------------------------------
+      app.post('/registrarCV', (req, res) => {
+        console.log(req.body.archivo);
+        res.redirect('/')
+        console.log(result)
+      })
+
+        
+  })
